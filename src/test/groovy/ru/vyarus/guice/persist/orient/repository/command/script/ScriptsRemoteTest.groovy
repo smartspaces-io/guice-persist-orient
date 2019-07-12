cf4a3787af2f0e3c4a9e028e477501b60940884e
Keith Hughes <kmhughes@users.noreply.github.com>
2019-07-12 12:28:52 -0600

    Removed use of dynamic providers for repositories. Added BND gradle plugin to make jar into an OSGi bundle.

--- src/test/groovy/ru/vyarus/guice/persist/orient/repository/command/script/ScriptsRemoteTest.groovy
+++
@@ -1,8 +0,0 @@
-package ru.vyarus.guice.persist.orient.repository.command.script
-
-/**
- * @author Vyacheslav Rusakov
- * @since 26.10.2017
- */
-class ScriptsRemoteTest extends ScriptsExecutionTest {
-}
--- src/test/groovy/ru/vyarus/guice/persist/orient/repository/delegate/ext/generic/support/OtherMixin.groovy
+++
@@ -1,9 +0,0 @@
-package ru.vyarus.guice.persist.orient.repository.delegate.ext.generic.support
-
-/**
- * @author Vyacheslav Rusakov 
- * @since 23.02.2015
- */
-interface OtherMixin<P> {
-
-}
